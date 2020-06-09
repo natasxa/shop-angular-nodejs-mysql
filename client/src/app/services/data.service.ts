6d135dead371ecb06fb73b48b99f9d06e56d5960
Natalia Salyaeva <56876800+natasxa@users.noreply.github.com>
2020-06-09 20:17:48 +0300

    Update data.service.ts

--- client/src/app/services/data.service.ts
+++ client/src/app/services/data.service.ts
@@ -8,43 +8,37 @@ export class DataService {
 
   public isLogged = new BehaviorSubject<boolean>(false)
   currentIsLogged = this.isLogged.asObservable();
-  changeIsLogged(l: boolean) {
-    console.log("isLogged is updated!")
+  changeIsLogged(l: boolean) {    
     this.isLogged.next(l)
   }
 
   public nameSourse = new BehaviorSubject<string>("guest")
   currentName = this.nameSourse.asObservable();
-  changeName(name: string) {
-    console.log("nameSourse is updated!")
+  changeName(name: string) {    
     this.nameSourse.next(name)
   }
 
   public total = new BehaviorSubject<number>(null)
   currentTotal = this.total.asObservable();
-  changeTotal(num: number) {
-    console.log("total is updated!")
+  changeTotal(num: number) {    
     this.total.next(num)
   }
 
   public itemsToShow = new BehaviorSubject<Array<any>>([])
   currentItemsToShow = this.itemsToShow
-  changeItemsToShow(data) {
-    console.log("itemsToShow is updated!")
+  changeItemsToShow(data) {    
     this.itemsToShow.next(data)
   }
 
   public itemsInCart = new BehaviorSubject<Array<any>>([])
   currentItemsInCart = this.itemsInCart
-  changeItemsInCart(data) {
-    console.log("itemsInCart is updated!")
+  changeItemsInCart(data) {   
     this.itemsInCart.next(data)
   }
 
   public itemToUpdate = new BehaviorSubject<Array<any>>([])
   currentItemToUpdate = this.itemToUpdate
-  changeItemToUpdate(data) {
-    console.log("itemToUpdate is choosed!")
+  changeItemToUpdate(data) {    
     this.itemToUpdate.next(data)
   }
 
