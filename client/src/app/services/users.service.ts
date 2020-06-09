7a743ce1f7ade34723e393affdb5bce60280ca1f
Natalia Salyaeva <56876800+natasxa@users.noreply.github.com>
2020-06-09 20:20:30 +0300

    Update users.service.ts

--- client/src/app/services/users.service.ts
+++ client/src/app/services/users.service.ts
@@ -10,24 +10,20 @@ export class UsersService {
   constructor(public http: HttpClient) { }
 
   public getAllUsers(): Observable<any> {
-    console.log("someone tries to get all users")
     return this.http.get('http://localhost:1000/users/',
       { responseType: "text" })
   }
   public getUserById(id): Observable<any> {
-    console.log("someone tries to check user ID")
     return this.http.get('http://localhost:1000/users/' + id,
       { responseType: "text" })
   }
   public login(body): Observable<any> {
-    console.log("someone tries to login")
     return this.http.post('http://localhost:1000/users/login', body, {
       headers: { 'Content-Type': 'application/json' },
       responseType: "text"
     })
   }
   public register(body): Observable<any> {
-    console.log("someone tries to register")
     return this.http.post('http://localhost:1000/users/register', body, {
       headers: { 'Content-Type': 'application/json' },
       responseType: "text"
